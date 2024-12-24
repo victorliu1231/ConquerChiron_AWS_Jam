720454eba21f54620686c3f29f842a7c18ea6c88
Victor Liu <victorliu1231@gmail.com>
2024-12-24 13:55:15 -0500

    updates

--- Assets/Scripts/ShipFront.cs
+++ Assets/Scripts/ShipFront.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ShipFront : MonoBehaviour {
     void OnTriggerEnter(Collider collider){
         if (collider.gameObject.tag == "Asteroid"){
-            collider.GetComponent<Fracture>().FractureObject();
             GameManager.Instance.ShipTakeDamage();
         }
     }
